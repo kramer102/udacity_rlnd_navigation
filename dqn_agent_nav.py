7a7b7653d6a4af342a8956b851f2f7f92c65f23b
Robert Kramer <kramer102@gmail.com>
2019-02-16 23:00:18 -0800

    feat: Modified DQN solution to train banana env

--- dqn_agent_nav.py
+++ dqn_agent_nav.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 from collections import namedtuple, deque
 
-from model import QNetwork
+from model_nav import QNetwork
 
 import torch
 import torch.nn.functional as F
